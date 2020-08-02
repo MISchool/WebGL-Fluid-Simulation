8dde7fb0ebbb1fbeb60344755af8664366c47649
AishaFisher <68912150+AishaFisher@users.noreply.github.com>
2020-08-02 17:05:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -24,18 +24,6 @@ SOFTWARE.
 
 'use strict';
 
-// Mobile promo section
-
-const promoPopup = document.getElementsByClassName('promo')[0];
-const promoPopupClose = document.getElementsByClassName('promo-close')[0];
-
-promoPopupClose.addEventListener('click', e => {
-    promoPopup.style.display = 'none';
-});
-
-const appleLink = document.getElementById('apple_link');
-
-const googleLink = document.getElementById('google_link');
 
 
 // Simulation section
